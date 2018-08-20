abf7fde53474a75b5340aed478b39b1a5233afee
Joe Zhou <joe@eniac.com.au>
2018-08-20 14:03:53 +1000

    React_Redux_Thunk template

---
+++ main.js
@@ -0,0 +1,17 @@
+// main.js
+
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './src/components/App.jsx';
+import reducer from './src/reducers';
+
+const store = createStore(reducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
